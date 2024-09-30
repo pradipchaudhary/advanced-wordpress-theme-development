c42d519ca6142da4639e566d183f886d699a92eb
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-09-30 19:23:51 +0545

    Create Project Schema

---
+++ models/Project.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema, model } from "mongoose";
+
+// Schema definition
+const projectSchema = mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    difficulty: {
+        type: String,
+        enum: ["Easy", "Medium", "Hard"],
+        required: true,
+    },
+    tech_used: {
+        type: [String],
+        required: true,
+    },
+    estimated_time: {
+        type: String,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+// Model
+const Project = mongoose.model("Project", projectSchema);
+
+export default Project;
