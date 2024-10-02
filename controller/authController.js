66f7dd625fd3d2955e5611ab66c30843a31b427d
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-10-03 01:10:32 +0545

    crete admin login controller

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,7 @@
-import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import { config } from "dotenv";
 
 export const registerAdmin = async (req, res) => {
     const { username, email, password } = req.body;
@@ -22,6 +24,39 @@ export const registerAdmin = async (req, res) => {
         await newUser.save();
         res.status(201).json({ message: "Admin user registered successfully" });
         console.log(newUser);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const loginAdmin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        // Find the user by email
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        // Check the password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        // Generate a JWT token
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        );
+        res.json({
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                role: user.role,
+            },
+        });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
