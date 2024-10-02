3342cee749d27e1fd0bc25669a889aadfac451c9
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-10-03 02:00:44 +0545

    fixed auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,28 @@
+import { config } from "dotenv";
+import jwt from "jsonwebtoken";
+
 export const isAuthenticated = (req, res, next) => {
     // Logic to authenticate user (JWT, session, etc.)
     // Populate req.user with user data
-    console.log("isAuthenticated...");
-    // call next middleware
-    next();
+
+    // Get the token from the Authorization header
+    const token = req.header("Authorization")?.split(" ")[1]; // Bearer <token>
+    console.log(token);
+
+    if (!token) {
+        return res
+            .status(401)
+            .json({ message: "Access denied. No token provided." });
+    }
+
+    try {
+        // Verify the token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Make sure to define JWT_SECRET in your environment variables
+        req.user = decoded; // Attach the decoded user info to the request
+        next(); // Call the next middleware or route handler
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid token." });
+    }
 };
 
 export const isAdmin = (req, res, next) => {
