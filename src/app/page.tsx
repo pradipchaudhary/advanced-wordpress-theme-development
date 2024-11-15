f593249b5221ee23471d23a82125582d1bc446e2
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-11-15 22:07:41 +0545

    feat: add SEO-(meta-gat)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <h1 className="text-3xl mb-4">Nex.js App Boilerplate </h1>
-            <p>This is your starting point for a scalable Next.js app!</p>
+            <h1 className="text-3xl mb-4">Pradip Chaudhary </h1>
+            <p>
+                Crafting seamless and dynamic web applications with React.js,
+                TypeScript, and modern web technologies.
+            </p>
         </div>
     );
 }
